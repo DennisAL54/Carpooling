5b9d85b9630b5acebc9382be3edfbe13d9916603
Unknown <alejimenezc@hotmail.com>
2018-11-16 23:04:05 -0600

    ...

--- app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ app/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -1,5 +1,22 @@
 package com.google.zxing.integration.android;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
 
 public class IntentIntegrator {
     
+
 }
