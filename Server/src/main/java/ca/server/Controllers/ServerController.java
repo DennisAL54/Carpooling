3a685053a33528eee4daabd08961d830972ce672
Unknown <alejimenezc@hotmail.com>
2018-11-21 17:00:56 -0600

    ...

--- Server/src/main/java/ca/server/Controllers/ServerController.java
+++ Server/src/main/java/ca/server/Controllers/ServerController.java
@@ -1,9 +1,16 @@
 package ca.server.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class ServerController {
-    
+
+    @GetMapping("/hello")
+    public String getHelloMessage(){
+        return "HOLA A TODOS";
+    }
+
+
 }
--- Server/src/main/java/ca/server/Model/Registro.java
+++ Server/src/main/java/ca/server/Model/Registro.java
@@ -1,4 +1,31 @@
 package ca.server.Model;
 
 public class Registro {
+
+    public Registro(long id, String name) {
+        this.id = id;
+        Name = name;
+    }
+
+    private long id;
+    private String Name;
+
+    public Registro() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
