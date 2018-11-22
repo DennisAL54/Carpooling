9adf715f1f7a43b9cfcff2147c85aed68edc9fee
Unknown <alejimenezc@hotmail.com>
2018-11-22 16:11:58 -0600

    ...

--- Server/src/main/java/ca/server/Controllers/ServerController.java
+++ Server/src/main/java/ca/server/Controllers/ServerController.java
@@ -9,27 +9,29 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 
-public class ServerController {
-    private List<Registro> registros = new ArrayList<>();
+public class ServerController {   // Controlador del server
+    private List<Registro> registros = new ArrayList<>(); //Crea una lista de personas registradas
     private AtomicLong nextID = new AtomicLong();
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") //Funcion para probar el server. Simplemente retorna un HOLA
     public String getHelloMessage(){
         return "HOLA A TODOS";
     }
-    @PostMapping("/registro")
+
+
+    @PostMapping("/registro")//Registra a una persona en el sistema
     public Registro CreateNewRegistro(@RequestBody Registro registro){
         registro.setId(nextID.incrementAndGet());
         registros.add(registro);
         return registro;
 
     }
-    @GetMapping("/registro")
+    @GetMapping("/registro")//Obtiene el registro completo de personas
     public List<Registro> getTregistros() {
         return registros;
     }
 
-    @GetMapping("/registro/{id}")
+    @GetMapping("/registro/{id}")//Retorna una persona en especifico
     public Registro GetOne(@PathVariable("id") long registroId){
         for(Registro registro : registros){
             if(registro.getId() == registroId){
@@ -38,7 +40,7 @@ public class ServerController {
         }
         throw new IllegalArgumentException();
     }
-    @PostMapping("/registro/{id}")
+    @PostMapping("/registro/{id}")// Edita una persona en especifico basandose en su ID
     public Registro editregistro(
             @PathVariable("id") long RegistroId,
             @RequestBody Registro newRegistro
