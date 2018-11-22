a4e3eb3c37c36ad6b3805cfe4347bc400db2bd89
Unknown <alejimenezc@hotmail.com>
2018-11-21 21:25:46 -0600

    Cambios menores al server

--- Server/src/main/java/ca/server/Controllers/ServerController.java
+++ Server/src/main/java/ca/server/Controllers/ServerController.java
@@ -1,10 +1,8 @@
 package ca.server.Controllers;
 
 import ca.server.Model.Registro;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -29,6 +27,15 @@ public class ServerController {
     @GetMapping("/registro")
     public List<Registro> getTregistros() {
         return registros;
+    }
+    @GetMapping("/registro/{id}")
+    public Registro GetOne(@PathVariable("id") long registroId){
+        for(Registro registro : registros){
+            if(registro.getId() == registroId){
+                return registro;
+            }
+        }
+        throw new IllegalArgumentException();
     }
 
 
