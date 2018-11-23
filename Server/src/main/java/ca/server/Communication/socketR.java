138f53b3d72879810899dce4d879b3ff9ce41f52
Unknown <alejimenezc@hotmail.com>
2018-11-22 18:23:23 -0600

    Socket de entrada

---
+++ Server/src/main/java/ca/server/Communication/socketR.java
@@ -0,0 +1,34 @@
+package ca.server.Communication;
+import java.net.*;
+import java.io.*;
+
+
+public class socketR {
+    ServerSocket server;
+    Socket socket;
+    int puerto = 9000;
+    DataOutputStream salida;
+    BufferedReader entrada;
+    public void iniciar()
+    {
+        try{
+            try {
+                server = new ServerSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            socket = new Socket();
+            try {
+                socket = server.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String mensaje = entrada.readLine();
+
+        }catch(Exception e){};
+
+    }
+
+
+}
